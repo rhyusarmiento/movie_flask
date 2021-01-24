257a8057957fa47f5d630eb176380d1954e8733a
rhyusarmiento <rhyu.sar.15@gmail.com>
2021-01-23 17:37:04 -0700

    Created app.sqlite

--- app.py
+++ app.py
@@ -5,9 +5,33 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def movie():
-    return "Hey Flask"
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), unique = False)
+    year = db.Column(db.Integer, unique = False)
+    rating = db.Column(db.Integer, unique = False)
+    genre = db.Column(db.String(50), unique = False)
+    starring = db.Column(db.String(50), unique = False)
+
+    def __init__(self, title, year, rating, genre, starring):
+        self.title = title
+        self.year = year
+        self.rating = rating
+        self.genre = genre
+        self.starring = starring
+
+
+class MovieSchema(ma.Schema):
+    class Meta:
+        fields = ('title', 'content')
+
+movie_schema = MovieSchema()
+movie_schema = MovieSchema(many = True)
 
 if __name__ == '__main__':
     app.run(debug = True)
